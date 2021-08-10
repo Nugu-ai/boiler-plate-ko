25df07f9573cb5d7a3741c23327d9b1a83023dd9
someone <someone@someplace.com>
2021-08-11 01:12:28 +0900

    ~logout

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const { User } = require("./models/User");
+const { auth } = require('./middleware/auth');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
@@ -25,7 +26,7 @@ app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요~~ 새해복 많이 받으세요~')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   //회원 가입할 때 필요한 정보들을 client에서 가져오면 
   //그것들을 데이터 베이스에 넣어준다.
 
@@ -37,7 +38,8 @@ app.post('/register', (req, res) => {
     })
   })
 })
-app.post('/login', (req, res) => {
+
+app.post('/api/users/login', (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if(!user) {
@@ -62,6 +64,32 @@ app.post('/login', (req, res) => {
     })
     }) 
 })
+
+app.get('/api/users/auth', auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true, // role이 0이면 일반 유저, 0이 아니면 admin(바뀔 수 있음)
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id}, 
+    { token: "" }
+    , (err, user) => {
+      if(err) return res.json({ success: false, err});
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
+
 
 
 
--- models/User.js
+++ models/User.js
@@ -44,7 +44,7 @@ userSchema.pre('save', function( next ){
             bcrypt.hash(user.password, salt, function(err, hash) {
                 if(err) return next(err)
                 user.password = hash
-                return next();
+                next();
             })
         });
     } else{
@@ -73,6 +73,22 @@ userSchema.methods.generateToken = function(cb) {
         if(err) return cb(err);
         cb(null, user)
     })
+}
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저 아이디를 이용해서 유저를 찾은 다음
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({ "_id": decoded, "token": token }, function(err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+
 }
 
 const User = mongoose.model('User', userSchema)
