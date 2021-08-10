6c02cd2a75e8fa89f7586fd9394a8ae6b83cd94c
someone <someone@someplace.com>
2021-08-10 21:00:13 +0900

    로그인까지

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const { User } = require("./models/User");
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 //application/x-www-form-urlencoded
@@ -9,6 +10,7 @@ app.use(express.urlencoded({extended: true}));
 
 //application/json
 app.use(express.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 const { urlencoded } = require('body-parser');
@@ -34,9 +36,34 @@ app.post('/register', (req, res) => {
       success: true
     })
   })
-
-
 })
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+      // 비밀번호까지 맞다면 토큰을 생성하기
+      user.generateToken( (err, user) => {
+        if(err) return res.status(400).send(err);
+      // 토큰을 저장한다. 어디에? 쿠키, 로컬 스토리지
+        res.cookie ("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
+    })
+    }) 
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
